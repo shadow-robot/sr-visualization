feda2876fb0375a5a15ae5be6b04a7011ba1b7bb
Ugo Cupcic <ugo@shadowrobot.com>
2013-02-15 11:32:09 +0100

    added possibility to run all available self tests - also folds down the previously run tests

--- sr_gui_self_test/src/sr_gui_self_test/self_test.py
+++ sr_gui_self_test/src/sr_gui_self_test/self_test.py
@@ -70,35 +70,49 @@ class SrGuiSelfTest(Plugin):
         self.on_btn_refresh_nodes_clicked_()
 
     def on_btn_test_clicked_(self):
-        #TODO: do this in a thread
-        self_test_srv = rospy.ServiceProxy(self.selected_node_+"/self_test", SelfTest)
-        resp = None
-        try:
-            resp = self_test_srv()
-        except rospy.ServiceException, e:
-            rospy.logerr("Failed to called " + self.selected_node_+"/self_test %s"%str(e))
-
-        node_item = None
-        if resp.passed == DiagnosticStatus.OK:
-            node_item = QTreeWidgetItem(["OK", self.selected_node_+"("+str(resp.id)+")"])
-        elif resp.passed == DiagnosticStatus.WARN:
-            node_item = QTreeWidgetItem(["WARN", self.selected_node_+"("+str(resp.id)+")"])
+        nodes_to_test = []
+        if self.selected_node_ == "All":
+            nodes_to_test = self.nodes[1:]
         else:
-            node_item = QTreeWidgetItem(["FAILED", self.selected_node_+"("+str(resp.id)+")"])
-        self._widget.test_tree.addTopLevelItem(node_item)
+            nodes_to_test = [self.selected_node_]
 
-        for status in resp.status:
-            display = ["", "", "", status.name, status.message]
-            if status.level == status.OK:
-                display[2] = "OK"
-            elif status.level == status.WARN:
-                display[2] = "WARN"
+        #fold previous tests
+        root_item = self._widget.test_tree.invisibleRootItem()
+        for i in range( root_item.childCount() ):
+            item = root_item.child(i)
+            item.setExpanded(False)
+
+        for n in nodes_to_test:
+            #TODO: do this in a thread
+            self_test_srv = rospy.ServiceProxy(n+"/self_test", SelfTest)
+            resp = None
+            try:
+                resp = self_test_srv()
+            except rospy.ServiceException, e:
+                rospy.logerr("Failed to called " + n+"/self_test %s"%str(e))
+            if resp == None:
+                rospy.logerr("Failed to called " + n+"/self_test %s"%str(e))
+                return
+
+            node_item = None
+            if resp.passed:
+                node_item = QTreeWidgetItem(["OK", n+"("+str(resp.id)+")"])
             else:
-                display[2] = "ERROR"
-            st_item = QTreeWidgetItem(node_item, display)
-            self._widget.test_tree.addTopLevelItem(st_item)
-            st_item.setExpanded(True)
-        node_item.setExpanded(True)
+                node_item = QTreeWidgetItem(["FAILED", n+"("+str(resp.id)+")"])
+            self._widget.test_tree.addTopLevelItem(node_item)
+
+            for status in resp.status:
+                display = ["", "", "", status.name, status.message]
+                if status.level == status.OK:
+                    display[2] = "OK"
+                elif status.level == status.WARN:
+                    display[2] = "WARN"
+                else:
+                    display[2] = "ERROR"
+                st_item = QTreeWidgetItem(node_item, display)
+                self._widget.test_tree.addTopLevelItem(st_item)
+                st_item.setExpanded(True)
+            node_item.setExpanded(True)
 
         for col in range(0, self._widget.test_tree.columnCount()):
             self._widget.test_tree.resizeColumnToContents(col)
@@ -109,6 +123,11 @@ class SrGuiSelfTest(Plugin):
 
         #gets all the list of services and only keep the nodes which have a self_test service
         self.nodes = [x[0].split('/')[1] for x in rosgraph.masterapi.Master("/").getSystemState()[2] if "self_test" in x[0]]
+
+        #uniquify items in the list
+        self.nodes = list(set(self.nodes))
+
+        self.nodes.insert(0, "All")
 
         self._widget.nodes_combo.clear()
         for node in self.nodes:
