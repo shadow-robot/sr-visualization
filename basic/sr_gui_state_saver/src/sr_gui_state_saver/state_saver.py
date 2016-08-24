37057de984b8e19542bc2abcac5cbfc03b070dcc
dg <dg@shadowrobot.com>
2016-08-24 17:57:45 +0100

    Fixed errors left as a result of copying an existing package to create this one. Tony has sharp eyes ;)

--- basic/sr_gui_state_saver/src/sr_gui_state_saver/state_saver.py
+++ basic/sr_gui_state_saver/src/sr_gui_state_saver/state_saver.py
@@ -4,7 +4,7 @@
 # inheriting from QObject
 #
 
-# Copyright 2011 Shadow Robot Company Ltd.
+# Copyright 2016 Shadow Robot Company Ltd.
 #
 # This program is free software: you can redistribute it and/or modify it
 # under the terms of the GNU General Public License as published by the Free
