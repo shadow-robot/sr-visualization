43e7a19cb09a2118903f0fb36cb38ba576d52388
michal <michal@shadowrobot.com>
2017-05-10 08:42:08 +0000

    checking jenkins test for minor change

--- basic/sr_gui_hand_calibration/test/test_hand_calibration.py
+++ basic/sr_gui_hand_calibration/test/test_hand_calibration.py
@@ -17,7 +17,8 @@ PKG = "sr_gui_hand_calibration"
 
 class TestHandCalibration(unittest.TestCase):
 
-    #def setUp(self):
+    def setUp(self):
+        self.tmp = 1
         #self.app = QApplication(sys.argv)
         #self._widget = QWidget()
         #ui_file = os.path.join(rospkg.RosPack().get_path('sr_gui_hand_calibration'), 'uis', 'SrHandCalibration.ui')
@@ -33,14 +34,15 @@ class TestHandCalibration(unittest.TestCase):
         #        """]}""")
         #f.close()
 
-    #def tearDown(self):
+    def tearDown(self):
+        self.tmp = 0
         #os.remove(rospy.get_param('mock_file'))
 
     def test_progress_bar(self):
         #self.assertEquals(self.hand_model.progress_bar.value(), 0)
         #self.hand_model.load(rospy.get_param('mock_file'))
         #self.assertEquals(self.hand_model.progress_bar.value(), 100)
-        self.assertEquals(1, 1)
+        self.assertEquals(self.tmp, 1)
 
 if __name__ == "__main__":
     rospy.init_node("test_hand_calibration")
