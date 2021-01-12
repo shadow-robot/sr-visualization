e87f9df7237d10544f4ab49784c4651fef074d7f
georgiablanco <georgia@shadowrobot.com>
2021-01-12 15:17:40 +0000

    fix

--- basic/sr_gui_hand_calibration/src/sr_gui_hand_calibration/sr_hand_calibration_model.py
+++ basic/sr_gui_hand_calibration/src/sr_gui_hand_calibration/sr_hand_calibration_model.py
@@ -239,7 +239,7 @@ class JointCalibration(QTreeWidgetItem):
 
         hand_finder = HandFinder()
         prefix = hand_finder.get_available_prefix()
-        print("The prefix: "prefix)
+        print("The prefix: " + prefix)
         if prefix == 'lh_':
             template = template.replace('/rh/', '/lh/')
         for replacement in replace_list:
